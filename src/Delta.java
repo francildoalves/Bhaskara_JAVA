ac233c82786200e961e2511d06e006dc98fdc961
FranCILDO Alves <53942335+francildoalves@users.noreply.github.com>
2021-11-12 17:42:38 -0300

    Update Delta.java

--- src/Delta.java
+++ src/Delta.java
@@ -3,14 +3,15 @@
  * @author FranCILDO Alves
  */
 
- private double a;
+ /*
+ private double LadoA;
  private double b;
  private double c;
+ */
      
- }
 
-public class Delta (){
-    public Double calculaDeltaDouble (a,  b, c){
+public class Delta(){
+    public static double calculaDeltaDouble (double a,  double b, double c){
         double delta = Math.pow(b, 2) - (4*a*c);
         return delta;
     }
