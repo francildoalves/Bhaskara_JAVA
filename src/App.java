ca3b64d9ac9805dcdd6e7462b08616f3dad15827
FranCILDO Alves <53942335+francildoalves@users.noreply.github.com>
2021-11-12 16:20:25 -0300

    Teste com OO

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author FranCILDO Alves
+ */
+
+ 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double a;
+        double b;
+        double c;
+
+        
+        Delta delta = new Delta();
+
+        a = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de A:"));
+        b = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de B:"));
+        c = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de C:"));
+        delta.calculaDeltaDouble(a, b, c);
     }
 }
--- src/Delta.java
+++ src/Delta.java
@@ -3,9 +3,16 @@
  * @author FranCILDO Alves
  */
 
+ private double a;
+ private double b;
+ private double c;
+     
+ }
+
 public class Delta (){
-    public Double calculaDeltaDouble (double a, double b, double c){
+    public Double calculaDeltaDouble (a,  b, c){
         double delta = Math.pow(b, 2) - (4*a*c);
         return delta;
     }
+
 }
