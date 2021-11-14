c1438ea7fff57135effc8cd15213f8d982da633d
FranCILDO Alves <53942335+francildoalves@users.noreply.github.com>
2021-11-14 19:19:43 -0300

    Update Delta.java

--- src/Delta.java
+++ src/Delta.java
@@ -18,7 +18,8 @@ public class Delta(){
      *
      * @param a - first numeric value for the ADD operation
      * @param b -  second numeric value for same purposes
-     * @return sum of two operands
+     * @param c -  third numeric value for same purposes
+     * @return a operação do valor de delta
      */
     public static double calculaDeltaDouble (double a,  double b, double c){
         double delta = Math.pow(b, 2) - (4*a*c);
