1d95bfea03a92a6f08990afadcfc97290fef6284
FranCILDO Alves <53942335+francildoalves@users.noreply.github.com>
2021-11-15 00:15:32 -0300

    Update Delta.java

--- src/Delta.java
+++ src/Delta.java
@@ -11,16 +11,28 @@
      
 
 public class Delta(){
-
-    /**
-     * 
-     * Adds two values.
+    
+    
+    /** 
+     * Subject line
+     *
+     * <p>Description of the method with optional {@code code} and {@link Object links to Javadoc}
+     * </p>
+     *
+     * <pre>
+     *    raw input
+     * </pre>
      *
      * @param a - first numeric value for the ADD operation
      * @param b -  second numeric value for same purposes
      * @param c -  third numeric value for same purposes
      * @return a operação do valor de delta
+     * @throws SomeException if bar goes wrong
+     * @see someOtherMethod()
+     * @since 2.2.2
+     * @author me
      */
+
     public static double calculaDeltaDouble (double a,  double b, double c){
         double delta = Math.pow(b, 2) - (4*a*c);
         return delta;
